21db40a52e08481478f687ce0215287a1724f5be
trickster1o1 <nischaltuladhar15@gmail.com>
2023-09-14 13:37:42 +0545

    booking page init

--- resources/views/Frontend/page/detail/Movie.blade.php
+++ resources/views/Frontend/page/detail/Movie.blade.php
@@ -1,23 +1,41 @@
 @extends('Frontend.layouts.app')
 @section('body')
-<div class="row movie-detail">
-    <div class="col-md-3 col-sm-12">
-        <div class="movie-box">
-            
-            <img src="{{ str_starts_with($page->thumb, 'http') ? $page->thumb : '/' . $page->thumb }}" alt="-"
-                class="i-fit">
+    <div class="row movie-detail">
+        <div class="col-md-3 col-sm-12">
+            <div class="movie-box">
+
+                <img src="{{ str_starts_with($page->thumb, 'http') ? $page->thumb : '/' . $page->thumb }}" alt="-"
+                    class="i-fit">
+            </div>
+            <small>{{ $page->status == 'active' ? 'NOW SHOWING' : 'COMMING SOON' }}</small>
+            <h4>{{ $page->title }}</h4>
+            <span class="movie-time">170 mins</span>
+            <div class="movie-desc">
+                {!! $page->description !!}
+            </div>
         </div>
-        <small>{{$page->status == 'active' ? 'NOW SHOWING' : 'COMMING SOON'}}</small>
-        <h4>{{$page->title}}</h4>
-        <span class="movie-time">170 mins</span>
-        <div class="movie-desc">
-            {!! $page->description !!}
+        <div class="col-md-9 col-sm-12">
+            <div class="movie-booking">
+                <div>
+                    <div>
+                        <h4>Available Seats</h4>
+                        <b>100</b>
+                    </div>
+                    
+                    <div>
+                        <h4>Price</h4>
+                        <b>Rs.{{$page->price}} / Seat</b>
+                    </div>
+                    <div>
+                        <h4>Location</h4>
+                        <b>Durbarmarg, Ktm</b>
+                    </div>
+                </div>
+
+                <div class="book-now">
+                    <button class="custom-btn">Book Now</button>
+                </div>
+            </div>
         </div>
     </div>
-    <div class="col-md-9 col-sm-12">
-        <div class="movie-booking">
-            okay here
-        </div>
-    </div>
-</div>
 @endsection
