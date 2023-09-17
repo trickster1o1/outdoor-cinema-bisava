8bf95a555b0596ed933bf24c50722918405be884
trickster1o1 <nischaltuladhar15@gmail.com>
2023-09-17 11:18:58 +0545

    checkout according to movie status

--- resources/views/Frontend/page/detail/Movie.blade.php
+++ resources/views/Frontend/page/detail/Movie.blade.php
@@ -7,7 +7,7 @@
                 <img src="{{ str_starts_with($page->thumb, 'http') ? $page->thumb : '/' . $page->thumb }}" alt="-"
                     class="i-fit">
             </div>
-            <small>{{ $page->status == 'active' ? 'NOW SHOWING' : 'COMMING SOON' }}</small>
+            <small>{{ $page->status == 'active' ? 'NOW SHOWING' : 'COMING SOON' }}</small>
             <h4>{{ $page->title }}</h4>
             <span class="movie-time">170 mins</span>
             <div class="movie-desc">
@@ -29,12 +29,17 @@
                 </div>
 
                 <div class="book-now">
-                    <form action="{{ Route('checkout.payment') }}" method="POST">
-                        @csrf
-                        <input type="hidden" value="{{$page->id}}" name="movie">
-                        <button class="custom-btn">Book Now</button>
+                    @if ($page->status == 'active')
+                        <form action="{{ Route('checkout.payment') }}" method="POST">
+                            @csrf
+                            <input type="hidden" value="{{ $page->id }}" name="movie">
+                            <button class="custom-btn">Book Now</button>
 
-                    </form>
+                        </form>
+                    @else
+                        <button class="custom-btn">Coming Soon</button>
+                    @endif
+
                 </div>
             </div>
         </div>
